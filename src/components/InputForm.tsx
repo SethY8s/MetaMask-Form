b4aa74a4546de8e2093b52f79c88afdc99a162a7
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-08-08 09:23:04 -0400

    basic form functions working and inputs are console.logging data

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input, Button } from '@web3uikit/core';
 
 export default function InputForm() {
+  const [address, setAddress] = useState('');
+  const [amount, setAmount] = useState('');
+
   return (
     <div className="input-form">
-      <Input size="large" type="text" placeholder="Address" />
-      <Input size="large" type="text" placeholder="Amount" />
+      <Input
+        value={address}
+        onChange={(event) => setAddress(event.target.value)}
+        size="large"
+        type="text"
+        placeholder="Address"
+      />
+      <Input
+        value={amount}
+        onChange={(event) => setAmount(event.target.value)}
+        size="large"
+        type="text"
+        placeholder="Amount"
+      />
       <div className="d-flex">
-        <Button text="clear" theme="secondary" />
-        <Button text="Send" size="large" theme="secondary" />
+        <Button
+          onClick={() => {
+            setAddress('');
+            setAmount('');
+          }}
+          text="clear"
+          theme="secondary"
+        />
+        <Button
+          onClick={() => {
+            const data = {
+              address,
+              amount,
+            };
+            console.log(data);
+          }}
+          text="Send"
+          size="large"
+          theme="secondary"
+        />
       </div>
     </div>
   );

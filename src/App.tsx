4086f45d1d6d4d0f8918bf14e4ea7abc4fd3f0aa
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-08-08 10:22:42 -0400

    added conditional that checks if user has metaMask wallet extension

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import InputForm from './components/InputForm';
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-100 d-flex justify-content-center">
       <InputForm />
     </div>
   );
--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 import { Input, Button } from '@web3uikit/core';
 
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
 export default function InputForm() {
   const [address, setAddress] = useState('');
   const [amount, setAmount] = useState('');
 
+  const submitFunction = () => {
+    if (!window.ethereum) {
+      alert('Please add MetaMask wallet extension to your browser');
+    }
+
+    const data = {
+      address,
+      amount,
+    };
+    console.log(data);
+  };
+
   return (
-    <div className="input-form">
+    <div className="d-flex flex-column align-items-center w-25 border rounded border-primary input-form">
       <Input
         value={address}
         onChange={(event) => setAddress(event.target.value)}
@@ -18,7 +36,7 @@ export default function InputForm() {
         value={amount}
         onChange={(event) => setAmount(event.target.value)}
         size="large"
-        type="text"
+        type="number"
         placeholder="Amount"
       />
       <div className="d-flex">
@@ -31,13 +49,7 @@ export default function InputForm() {
           theme="secondary"
         />
         <Button
-          onClick={() => {
-            const data = {
-              address,
-              amount,
-            };
-            console.log(data);
-          }}
+          onClick={() => submitFunction()}
           text="Send"
           size="large"
           theme="secondary"
